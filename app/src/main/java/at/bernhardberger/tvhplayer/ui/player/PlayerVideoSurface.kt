1dea7395ea252acfa8cce33498c6d4bd7aafdb48
Bernhard Berger <bernhard.berger@gmail.com>
2026-07-29 20:54:52 +0200

    Harden PlayerView surface lifecycle

--- app/src/main/java/at/bernhardberger/tvhplayer/ui/player/PlayerVideoSurface.kt
+++ app/src/main/java/at/bernhardberger/tvhplayer/ui/player/PlayerVideoSurface.kt
@@ -1,5 +1,6 @@
 package at.bernhardberger.tvhplayer.ui.player
 
+import android.view.View
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
@@ -29,6 +30,11 @@ fun PlayerVideoSurface(
                     this.player = player
                     useController = false
                     controllerAutoShow = false
+                    isFocusable = false
+                    isFocusableInTouchMode = false
+                    isClickable = false
+                    importantForAccessibility =
+                        View.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS
                     keepScreenOn = true
                 }
             },
@@ -46,6 +52,10 @@ fun PlayerVideoSurface(
                     AspectRatioMode.FORCE_4_3 -> Modifier.aspectRatio(4f / 3f)
                 }
             ),
+            onRelease = { view ->
+                view.player = null
+                view.keepScreenOn = false
+            },
         )
         DebugVideoBackdrop(
             visible = debugVideoBackdropVisible,
